eac9bfb5bdef50b0db4cc3c0b85f696eb4238116
pawlo555 <spawel555@outlook.com>
2020-11-15 21:04:58 +0100

    Adding first test
    Add test to noe_or_edge function

--- testy.py
+++ testy.py
@@ -2,11 +2,18 @@
 """
 Created on Sun Nov 15 20:45:50 2020
 
-@author: spawe
+@author: Paweł Świder
 """
 
 from Graph import *
+import unittest
 
-edge = "\t10 -- 9"
+class GraphTest(unittest.TestCase):
 
-print(node_or_edge(edge))
+    def test_node_or_edge(self):
+        self.assertFalse(node_or_edge("\t10 -- 6"))
+        self.assertTrue(node_or_edge("\t10 [label=Y]"))
+
+
+if __name__ == '__main__':
+    unittest.main()
