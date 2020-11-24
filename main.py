14224b70e362e012729e9eb6dbb397a7a0ad1b2c
jupiterb <72196454+jupiterb@users.noreply.github.com>
2020-11-24 01:39:47 +0100

    MAIN

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 """
 Created on Sat Nov 14 2020
 
-@author: Paweł Świder
-"""
+@author: Piotr Biały
 
 from Graph import Graph_Transformation
 from Production import Production
@@ -28,3 +27,59 @@ G3 = production_1.produce(G2, "wynik2")
 G4 = production_1.produce(G3, "wynik3")
 G5 = production_1.produce(G4, "wynik5")
 G5.view()
+"""
+
+from Production import Production
+from read_Graph import *
+
+
+def parse_args():
+    command = input()
+    args = command.split()
+    # name of output graph is args[0]
+    name = args[0]
+    # path to input graph is args[1]
+    path = args[1]
+    # others args are names of productions
+    production_names = args[2:,]
+
+    production_paths = [["productions\Left_" + name + ".dot",
+                         "productions\Right_" + name + ".dot"
+                         "productions\EmbedT_" + name + ".txt"]
+                        for name in range(len(production_names))]
+    return name, path, production_paths
+# end def
+
+
+def get_graph(name, path):
+    return graph_to_graph_transformation(name,read_Graph(name,path))
+# end def
+
+
+def embed_transformation(path):
+    file = open(path, "r")
+    embed_transformation = {}
+    with file:
+        embed_transformation.fromkeys(file.read(), file.read())
+    file.close()
+    return embed_transformation
+# end def
+
+
+def get_productions(production_paths):
+    productions = [[None]*3 for _ in production_paths]
+    for i, production in enumerate(production_paths):
+        productions[i][0] = read_Graph(str(i)+"R", production[0])
+        productions[i][1] = read_Graph(str(i)+"L", production[1])
+        productions[i][2] = embed_transformation(production[2])
+    return productions
+# end def
+
+
+name, g_path, p_paths = parse_args()
+graph = get_graph(name, g_path)
+prod_args = get_productions(p_paths)
+for prod_arg in prod_args:
+    production = Production(prod_arg[0], prod_arg[1], prod_arg[2])
+    graph = production.produce(graph, "name")
+graph.render(filename="results\\" +  name + ".dot")
---
+++ read_Graph.py
@@ -0,0 +1,23 @@
+from Graph import Graph_Transformation
+import pydot
+from graphviz import Graph
+
+def read_Graph(name,path):
+    graph = pydot.graph_from_dot_file(path)[0]
+    graph_g = Graph(name)
+
+    for node in graph.get_nodes():
+        graph_g.node(node.get_name(), node.get('label'))
+
+    for edge in graph.get_edges():
+        graph_g.edge(edge.get_source(), edge.get_destination())
+
+    return graph_g
+# end def
+
+
+def graph_to_graph_transformation(name,graph_g):
+    graph_t = Graph_Transformation(name)
+    graph_t.body = graph_g.body
+    return graph_t
+# end def
