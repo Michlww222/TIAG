7b9f88bd5c50e0325306f000dac1c06b358444da
pawlo555 <spawel555@outlook.com>
2020-12-07 17:04:59 +0100

    Graph refactor
    Changing desciption into english.
    Deleting some unneeded prints

--- Graph.py
+++ Graph.py
@@ -8,49 +8,33 @@ Created on Sat Nov 14 2020
 from graphviz import Graph
 
 class Graph_Transformation(Graph):
-    """Klasa wykonująca produkcje na grafie"""
+    """Class extending Graph class from Graphviz"""
     
-    def Graph_Transformation(self, name):
-        """Inicjuje graf
-        name - nazwa naszego grafu oraz pliku pdf w wygenerowanym grafem
+    def __init__(self, name):
+        """
+        Initiate a graph
+        name - name of our graph
         """
         super().__init__(name, filename=name)
-            
-    def HelloGraph(self):
-        self.node("A");
-        self.node("B");
-        self.edge("A","B");
-        self.view()
-        
-    def find_node_with_label(self,label):
-        """Znajduje wierzchołek z o określonym parametrze label
-        label - nazwa parametru label wierzchołka
-        return - string zawierający informacje o wierzchołku
-        DO USUNIĘCIA
-        """
-        for elem in self.body:
-            if ' -- ' not in elem:
-                if get_label(elem) == label:
-                    return elem
-        return None
     
     def find_nodes_with_label(self,label):
-        """Znajduje wierzchołki z o określonym parametrze label
-        label - nazwa parametru label wierzchołka
-        return - lista wierzchołków o podanym paramentze label
+        """
+        Finds nodes with set label
+        Label - label we use to find nodes with this label
+        return - nodes with label
         """
         nodes = []
         for elem in self.body:
-            if ' -- ' not in elem:
+            if node_or_edge(elem):
                 if get_label(elem) == label:
                     nodes.append(elem)
-        print(nodes)
         return nodes
     
     def find_edges_to_node(self, name):
-        """Znajduje listę krawędzi powiędzy wierzchołkami
-        name - nazwa krawędzi
-        raturn - lista krawędzi z wierzchołkiem name
+        """
+        Finds edges which has one common node
+        name - name of node
+        return - list of edges containing node with name
         """
         list_of_edges = []
         for elem in self.body:
@@ -60,12 +44,12 @@ class Graph_Transformation(Graph):
         return list_of_edges
     
     def translate_graph(self, translator, graph_name):
-        """Generuje nowy graf izomorficzny
-        translator - słownik zawierający gdzie klucz to stara wartosć a pole nowa
+        """
+        Create a new isomorphic graph
+        translator - dictionary used as translator
         translator[old_name] = new_name
-        lista nowych nazw
-        graph_name - nazwa nowego izomorficznego grafu
-        return - graf izomorficzny G2
+        graph_name - name of new isomorphic graph
+        return - new isomorphic graph
         """
         G2 = Graph_Transformation(graph_name)
         nodes, edges = self.find_nodes_and_edges()
@@ -85,9 +69,11 @@ class Graph_Transformation(Graph):
         return G2
     
     def find_labels(self,nodes_names):
-        """Znajduje wartosci label dla podanych nazw nodów
-        nodes_names - lista nazw wierzchołków
-        return - lista imion dla poszczególnych nodóW
+        """
+        Finds labes to list of nodes names in graph
+        nodes_names - lista of nodes names
+        return - list of labels 
+        nodes_names[i] - labels[i] -- name and label of the same node
         """
         nodes, _ = self.find_nodes_and_edges()
         labels = [None]*len(nodes_names)
@@ -97,17 +83,14 @@ class Graph_Transformation(Graph):
                 labels[node_position] = get_label(node)
             except:
                 None
-        
         if None in labels:
-            print(nodes_names)
-            print(labels)
             raise ValueError("W tabeli label jest None")
-
         return labels
     
     def find_nodes_and_edges(self):
-        """Zwraca listę wierzchołków i listę krawędzi
-        return - (nodes, edges)
+        """
+        Finds graph nodes and edges
+        return - (nodes, edges) - nodes and edges are lists
         """
         nodes = []
         edges = []
@@ -117,38 +100,40 @@ class Graph_Transformation(Graph):
             else:
                 edges.append(element)
         return nodes,edges
-
-
-
+    
 def node_or_edge(element):
-    """Sprawdza czy element jest wierzchołkiem czy grawędzią
-    element - string reprezentujący linię tekstu w formacie dot
-    return - true -> wierzchołek, false -> krawędź
+    """
+    Check if the graph element is node or edge
+    element - graph element we want to check
+    return - True -> node, False -> edge
     """
     return ' -- ' not in element
     
 def get_label(node):
-    """Zwraca wartość label dla danego wierzchołka
-    node - wierzchołek zapisany w formacie dot
-    Return: zawartość parametru label
+    """
+    Get label of the node
+    node - graphviz node -- "1 [label=Y]
+    return - node label
     """
     start = node.find('label=') + 6
     end = node.find(']')
     return node[start:end]           
 
 def get_name(node):
-    """Zwraca unikalna nazwę wierzchołka
-    node - wierzchołek zapisany w formacie dot
-    Return: unikalna nazwa wierzchołka
+    """
+    Get name of the node
+    node - graphviz node -- "1 [label=Y]
+    return - node name
     """
     last = node.find('[')
     return node[1:last-1]
 
 def find_unique_names(names, n):
-    """Znajduje odpowiednie nazwy dla wierzchołków
-    names - posortowana tablica liczb zawierająca już używane nazwy
-    n - ilosć nazw do znalezienia
-    return - tablica string nazwierająca odpowiednia nazwy
+    """
+    Finds proper names for the nodes
+    names - sorted list of forbidden names
+    n - amount of the names we want to find
+    return - list of string with proper nodes names for new nodes
     """
     i = 0
     j = 0
@@ -167,9 +152,10 @@ def find_unique_names(names, n):
     return new_names
     
 def get_names_from_edge(edge):
-    """Zwraca nazwy wierzchołków dla danej krawędzi
-    edge - krawędź
-    return - (a,b), gdzie a,b to nazwy wierzchołków połączonych krawędzią edge
+    """
+    Get nodes names from the edge
+    edge - edge
+    return - tuple (a,b), a and b are nodes names
     """
     first_end = edge.find(' -- ')
     first_start = 1
